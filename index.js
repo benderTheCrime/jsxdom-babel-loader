fa7e67a09b37bfae865d4fdffa27b32e19054f85
Joe Groseclose <joegroseclose@Joes-MacBook-Pro.local>
2016-01-06 15:19:02 -0500

    Modified main file to call babel, added babel to dependencies

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-var loaderUtils = require('loader-utils');
-var jsxdom = require('jsxdom');
+var loaderUtils = require('loader-utils'),
+    jsxdom = require('jsxdom'),
+    babel = require('babel-core');
 
 module.exports = function(source) {
-  this.cacheable && this.cacheable(true);
-
-  var query = loaderUtils.parseQuery(this.query);
-  var tree = [
-    "require('jsxdom/dist/appendChildren.js');",
-    "require('jsxdom/dist/setAttributes.js');",
-    jsxdom.transpile(source, query)
-  ];
-
-  return tree.join('\n');
+    this.cacheable && this.cacheable(true);
+    var query = loaderUtils.parseQuery(this.query),
+        tree = [
+            "require('jsxdom/dist/appendChildren.js');",
+            "require('jsxdom/dist/setAttributes.js');",
+            jsxdom.transpile(source, query)
+        ];
+    return babel.transform(tree.join('\n'), query.babel).code;
 };
